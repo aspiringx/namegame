aeb2a25a274f9af38d03e1121ec15c2427aec592
Joe Tippetts <joe@fonespark.com>
2025-08-10 15:54:14 -0600

    Add nextBuildId to avoid failed to find server action in server logs

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
+const nextBuildId = require('next-build-id')
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  generateBuildId: () => nextBuildId({ dir: __dirname }),
   images: {
     deviceSizes: [320, 480, 640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
