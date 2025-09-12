0602598a5ac08be2e1445341e00c678fb45b04ca
Joe Tippetts <joe@fonespark.com>
2025-09-12 15:39:23 -0600

    ui working but console and server log errors

--- src/app/g/[slug]/games/page.tsx
+++ src/app/g/[slug]/games/page.tsx
@@ -2,7 +2,6 @@ import { getGroupForLayout } from '../utils'
 import GamesPageClient from './GamesPageClient'
 import { notFound } from 'next/navigation'
 
-export const dynamic = 'force-dynamic'
 
 export default async function GamesPage({ params }: { params: { slug: string } }) {
   const groupData = await getGroupForLayout(params.slug)
--- src/app/g/[slug]/utils.ts
+++ src/app/g/[slug]/utils.ts
@@ -1,21 +1,24 @@
+import { cache } from 'react'
 import { getGroupTypeBySlug } from './data'
-import { getGroup as getAllGroup } from './(community)/data'
+import { getGroup as getCommunityGroup } from './(community)/data'
 import { getGroup as getFamilyGroup } from './(family)/data'
 import { FamilyGroupData, CommunityGroupData } from '@/types'
 
 // Helper to fetch the correct group data based on type
-export const getGroupForLayout = async (
-  slug: string,
-  limit?: number,
-): Promise<CommunityGroupData | FamilyGroupData | null> => {
-  const groupTypeData = await getGroupTypeBySlug(slug)
-  if (!groupTypeData) {
-    return null
-  }
+export const getGroupForLayout = cache(
+  async (
+    slug: string,
+    limit?: number,
+  ): Promise<CommunityGroupData | FamilyGroupData | null> => {
+    const groupTypeData = await getGroupTypeBySlug(slug)
+    if (!groupTypeData) {
+      return null
+    }
 
-  if (groupTypeData.groupType.code === 'family') {
-    return getFamilyGroup(slug, limit)
-  }
+    if (groupTypeData.groupType.code === 'family') {
+      return getFamilyGroup(slug, limit)
+    }
 
-  return getAllGroup(slug, limit)
-}
+    return getCommunityGroup(slug, limit)
+  },
+)
